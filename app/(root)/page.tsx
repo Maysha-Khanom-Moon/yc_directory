076aa4d32b36ab9a7c1f158dae69e8f80849d0fb
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-08-28 02:36:24 +0600

    startup card's image and description added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,7 +11,7 @@ export default async function Home({ searchParams }: PageProps) {
   const posts = [{
     _createdAt: new Date(),
     views: 55,
-    author: { _id: 1 },
+    author: { _id: 1, name: 'John Doe' },
     _id: 1,
     description: 'This is a sample description',
     image: 'https://plus.unsplash.com/premium_photo-1674389991678-0836ca77c7f7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,6 +1,8 @@
 import { formatDate } from '@/lib/utils';
 import { EyeIcon } from 'lucide-react';
+import Link from 'next/link'; 
 import React from 'react';
+import Image from 'next/image';
 
 type Post = {
   _id: string | number;
@@ -10,22 +12,48 @@ type Post = {
   image: string;
   category: string;
   views: number;
-  author: { _id: string | number };
+  author: { _id: string | number, name: string };
 };
 
 const StartupCard = ({ post }: { post: Post }) => {
+
+  const { _createdAt, views, author: { _id: authorId, name }, title, category, _id, image, description } = post;
+
   return (
-    <li className="startup-card">
+    <li className="startup-card group">
       <div className="flex-between">
         <p className="startup-card_date">
-          {formatDate(post._createdAt.toString())}
+          {formatDate(_createdAt.toString())}
         </p>
 
         <div className='flex gap-1.5'>
           <EyeIcon className='w-6 h-6 text-primary' />
-          <span className='text-16-medium'>{post.views}</span>
+          <span className='text-16-medium'>{views}</span>
         </div>
       </div>
+
+      <div className='flex-between gap-5 mt-5'>
+        <div className='flex-1'>
+          <Link href={`/user/${authorId}`}>
+            <p className='text-16-medium line-clamp-1 text-black'>{name}</p>
+          </Link>
+          <Link href={`/startup/${_id}`}>
+            <h3 className='text-26-semibold line-clamp-1'>{title}</h3>
+          </Link>
+        </div>
+
+        <Link href={`/user/${authorId}`}>
+          <Image src={image} alt={name} width={80} height={80} className='w-20 h-20 rounded-full object-cover' />
+        </Link>
+      </div>
+
+      <Link href={`/startup/${_id}`}>
+        <p className='startup-card_desc'>
+          {description}
+        </p>
+
+        <img src={image} alt={title} className='startup-card_img' />
+      </Link>
     </li>
   );
 };
