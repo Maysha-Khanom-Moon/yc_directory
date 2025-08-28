4d15954fc0a25dce2ff649855ef16d7e571ecbd4
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-08-28 17:14:21 +0600

    get instant update, not from the cache. Although cache is faster

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,5 +6,6 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
+  // Although cache is more faster but delay for 60 sec to update newly added conten
   useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
